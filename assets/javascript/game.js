e8fcbe769d2a3b41e19d78555b13f38b74086aeb
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-03-11 23:18:02 -0700

    Basic Structure, Made word list and variables. Added Divs and titles to page

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// List of Fruit
+var wordList = [
+ "banana",
+ "apple",
+ "grapes"
+ "strawberry",
+ "watermelon",
+]
+
+// Variables
+var chosenWord = "";
+var letterInChosenWord = [];
+var numBlanks = 0;
+var blanks = [];
+var wrongGuesses = [];
+var winCounter = 0;
+var lossCounter = 1;
+var numOfGuesses = 15;
+
+}
