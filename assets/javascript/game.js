d99092ec7cde569383d56fa1d7489fc9390ea192
Jessie Ramirez <jessieramirez@Jessies-MacBook-Air.local>
2018-03-15 18:13:22 -0700

    Best copy. Edited images and alerts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,45 @@
-// list of fruits
+// arrays
 var wordList = [
  "banana",
  "pear",
+ "raspberry",
  "strawberry",
  "watermelon",
  "cherry",
  "peach",
  "oranges"
 ]
+
+var keyCodes = {
+  65: "a", 
+  66: "b", 
+  67: "c", 
+  68: "d", 
+  69: "e", 
+  70: "f", 
+  71: "g", 
+  72: "h", 
+  73: "i", 
+  74: "j", 
+  75: "k", 
+  76: "l", 
+  77: "m", 
+  78: "n", 
+  79: "o", 
+  80: "p", 
+  81: "q", 
+  82: "r", 
+  83: "s", 
+  84: "t", 
+  85: "u", 
+  86: "v", 
+  87: "w", 
+  88: "x", 
+  89: "y", 
+  90: "z",
+  91: "_"
+}
+
 
 // variables
 var chosenWord = "";
@@ -19,9 +51,6 @@ var winCounter = 0;
 var lossCounter = 1;
 var numOfGuesses = 15;
 
-function restartGame(){
-   document.getElementById("button") = gameStart() ;
-}
 
 function gameStart(){
   wrongGuesses = [];
@@ -39,7 +68,6 @@ function gameStart(){
   for(var i = 0; i < numBlanks; i++){
       blanks.push("_");
 }
-
   document.getElementById('wordBlank').innerHTML = blanks.join(" ");
   document.getElementById('guessesLeft').innerHTML = numOfGuesses;
 }
@@ -51,7 +79,6 @@ function letterCheck(letter){
     for(var i = 0; i < numBlanks; i++){
         if(chosenWord[i] === letter){
             letterInWord = true;
-
         }
     }
 
@@ -59,14 +86,13 @@ function letterCheck(letter){
         for(i = 0; i < numBlanks; i++){
             if(chosenWord[i] === letter){
             blanks[i] = letter;
-
         }
-
-        }
-    }else{
-        numOfGuesses --;
-        wrongGuesses.push(letter)
     }
+
+      }else{
+          numOfGuesses --;
+          wrongGuesses.push(letter)
+      }
 }
 
 
@@ -77,22 +103,29 @@ function roundComplete(){
 
     if(lettersInChosenWord.join(" ") === blanks.join(" ")){
         winCounter++;
-        // alert("You win!!");
+        document.getElementById("image-div").style.visibility = "visible";
         document.getElementById('winCounter').innerHTML = winCounter;
-        // gameStart();
+        $("#image-div").html("<img src='assets/images/fruitBowl.png' />"); 
+        $("#guessesLeft").off(onkeydown);
+        gameStart(); 
+
     }else if(numOfGuesses === 0){
         document.getElementById('lossCounter').innerHTML  = lossCounter ++;
         document.getElementById('wrongGuesses').innerHTML = "";
-        alert("Game Over");      
-        gameStart();
+        $("#image-div").html("<img src='assets/images/sadBowl.png' />");
+        console.log("Game Over");
+        gameStart();      
     }
 
 }
 
-function restartGame(){
-   document.getElementById("button") = gameStart() ;
+// Click to restart game
+function restartGame() {
+    $("#image-div").html("");
+    gameStart();
 
-};
+}
+
 
 gameStart();
 document.onkeydown = function(event){
@@ -100,4 +133,7 @@ document.onkeydown = function(event){
     letterCheck(letterGuessed)
     roundComplete();
 }
+
+
+
 
